ad92345a347a748806a488819ce644ed0b2b5f7c
Filip746 <fkvesic@mathos.hr>
2025-02-24 21:49:31 +0100

    Edited index.html #3

--- app.py
+++ app.py
@@ -12,21 +12,20 @@ headers = {
     'Authorization': f'Bearer {AZURE_ML_API_KEY}'
 }
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
     input_data = {"data": [[data['param1'], data['param2'], data['param3']]]}
-
-    response = requests.post(AZURE_ML_ENDPOINT, json=input_data, headers=headers)
-
+    
+    response = requests.post(AZURE_ML_ENDPOINT, 
+                            json=input_data, 
+                            headers=headers)
+    
     if response.status_code == 200:
         return jsonify(response.json())
     else:
         return jsonify({'error': 'API request failed'}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
